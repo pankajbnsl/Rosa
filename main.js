7b5fc97afc7b694933a930f5fcebd97b75ae622d
pankajbnsl <luckypnkj21@gmail.com>
2020-09-13 23:18:52 +0530

    complete html and css part

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ let body = selectElement('body');
 menuToggler.addEventListener('click', function(){
 	body.classList.toggle('open');
 });
+
+
+window.sr = scrollReveal();
+
+sr.reveal('.animate-left',{
+	origin: 'left',
+	duration: 1000,
+	distance: '25rem',
+	delay: 300
+});
+
+sr.reveal('.animate-right',{
+	origin: 'right',
+	duration: 1000,
+	distance: '25rem',
+	delay: 600
+});
